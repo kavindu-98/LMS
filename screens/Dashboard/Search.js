8df7b4a2148e4c75ea8414fd4b477f6cfbd72568
kavindu-98 <kavinduumayanga058@gmail.com>
2022-10-16 19:08:47 +0530

    Search Screen

--- screens/Dashboard/Search.js
+++ screens/Dashboard/Search.js
@@ -4,9 +4,19 @@ import {
     Text,
     Image,
     TextInput,
-    FlatList
+    FlatList,
+    ScrollView
 } from 'react-native';
 import { Shadow } from 'react-native-shadow-2';
+
+import Animated, {
+            Extrapolate,
+            interpolate,
+            useAnimatedScrollHandler,
+            useAnimatedStyle,
+            useSharedValue,
+            
+} from 'react-native-reanimated';
 
 import {
     IconButton,
@@ -29,8 +39,89 @@ import{
 
 
 const Search = () => {
+
+    const scrollViewRef = React.useRef() 
+    function renderTopSearches() {
+        return (
+            <View 
+                style={{
+                    marginTop: SIZES.padding
+                }}
+                >
+                    <Text
+                        style={{
+                            marginHorizontal: SIZES.padding,
+                            ...FONTS.h2
+                        }}
+                    >
+
+                        All Courses
+                    </Text>
+                    <FlatList
+                        horizontal
+                        data={dummyData.top_searches}
+                        listKey="TopSearches"
+                        scrollEnabled={false}
+                        keyExtractor={item => 'TopSearches-${item.id}'}
+                        showsHorizontalScrollIndicator={false}
+                        contentContainerStyle={{
+                            marginTop: SIZES.radius,
+                        }}
+                        renderItem={({ item, index }) => (
+                                <TextButton
+                                    label={item.label}
+                                    contentContainerStyle={{
+                                        paddingVertical: SIZES.radius,
+                                        paddingHorizontal: SIZES.padding,
+                                        marginLeft: index == 0 ? SIZES.padding : SIZES.radius,
+                                        marginRight: index == dummyData.top_searches.length - 1 ? SIZES.padding : 0,
+                                        borderRadius: SIZES.radius,
+                                        backgroundColor: COLORS.gray10
+                                    }}
+                                    labelStyle={{
+                                        color: COLORS.gray50,
+                                        ...FONTS.h3
+
+                                    }}
+                                
+                                >
+                
+                                </TextButton>
+                        )}
+                        
+                        
+                
+                        />
+
+
+                </View>
+        )
+    }
+
+
     return (
-        <View>
+        <View
+            style={{
+                flex: 1,
+                backgroundColor: COLORS.white
+            }}
+        >
+            <Animated.ScrollView
+                ref={scrollViewRef}
+                contentContainerStyle={{
+                    marginTop: 100,
+                    paddingBottom: 300
+                }}
+                showsVerticalScrollIndicator={false}
+                scrollEventThrottle={16}
+                keyboardDismissMode="on-drag"
+            >
+                 {/* Top Search */}
+                {renderTopSearches()}
+
+
+
+            </Animated.ScrollView>
             <Text>Search</Text>
         </View>
     )
